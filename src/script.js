fd6cd1084db9884a57b94a50f689a0951b40140a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-06-16 20:04:13 +0530

    Few Improvements

--- src/script.js
+++ src/script.js
@@ -2,27 +2,27 @@ document.addEventListener("DOMContentLoaded", () => {
     const modal = document.querySelector(".modal");
     const modalContent = document.querySelector(".modal-content");
     const btn = document.querySelector(".btn");
+    const closeBtn = document.querySelector(".close");
 
     btn.addEventListener("click", (e) => openModal(e));
+    closeBtn.addEventListener("click", closeModal);
 
     modal.addEventListener("click", (e) => {
-        const evt = e.target.classList;
-        if (evt.contains("modal") || evt.contains("close")) {
+        if (e.target === modal) {
             closeModal();
         }
     });
 
     function openModal(e) {
         e.preventDefault();
-        modal.style.display = "block";
+        modal.classList.add("show");
     }
 
     function closeModal() {
         modalContent.classList.add("slide-up");
-        
         setTimeout(() => {
-            modal.style.display = "none";
+            modal.classList.remove("show");
             modalContent.classList.remove("slide-up");
-        }, 500);
+        }, 500); // Match the duration of the slide-up animation
     }
 });
