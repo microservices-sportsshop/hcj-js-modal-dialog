e13944be08e75a8a7ec26f2a0ddeb00ef41a4819
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-06-16 19:39:17 +0530

    Changes

--- src/script.js
+++ src/script.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    const modal = document.querySelector(".modal");
+    const btn = document.querySelector(".btn");
+
+    btn.addEventListener("click", (e) => openModal(e));
+
+    modal.addEventListener("click", (e) => {
+        const evt = e.target.classList;
+        if (evt.contains("modal") || evt.contains("close")) {
+            closeModal();
+        }
+    });
+
+    function openModal(e) {
+        e.preventDefault();
+        modal.style.display = "block";
+    }
+
+    function closeModal() {
+        modal.style.display = "none";
+    }
 });
